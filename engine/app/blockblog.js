483eff0105a49f0d0b62317dd0da377dea87fe18
Hyunsang Sun <asunhs@gmail.com>
2015-10-26 20:45:34 +0900

    준비 끝

--- engine/app/blockblog.js
+++ engine/app/blockblog.js
@@ -5,50 +5,34 @@ var Tracker = require('./tracker.js');
 
 
 
-
-function toHtml(doc, blockblog) {
-    var lines = doc.toString().split('\r\n'),
-        result = [],
-        tracker = new Tracker(function (type, block) {
-            console.log(type);
-            result.push(blockblog.run(type, block));
-        }),
-        i;
-
-    for (i=0; i<lines.length; ++i) {
-        tracker.append(lines[i]);
-    }
-
-    tracker.flush();
-
-    return result.join('\r\n');
-}
-
-
-
 function BlockBlog() {
     this.rules = {};
 }
 
+BlockBlog.SPLITER = /^([^:]+)(:(.*))?$/;
+
 BlockBlog.prototype = {
-    addRule: function (name, fn) {
-        this.rules[name] = fn;
+    addRule: function (type, fn) {
+        this.rules[type] = fn;
     },
     loadRule: function (path) {
         var loader = require(path);
         return loader(this);
     },
     run: function (name, block) {
-        return this.rules[name] && this.rules[name](block);
+        var tokens = name.match(BlockBlog.SPLITER),
+            type = tokens[1],
+            param = tokens[3];
+
+        return this.rules[type] && this.rules[type](block, param);
     },
     convert : function (src, dest, callback) {
-
         var blockblog = this;
 
         return fs.readFile(path.normalize(src), function (err, data) {
             if (err) throw err;
 
-            var converted = toHtml(data, blockblog);
+            var converted = blockblog.toHtml(data);
 
             return fs.writeFile(path.normalize(dest), converted, function (err) {
                 if (err) throw err;
@@ -56,6 +40,24 @@ BlockBlog.prototype = {
                 return callback();
             });
         });
+    },
+    toHtml: function(doc) {
+        var blockblog = this,
+            lines = doc.toString().split('\r\n'),
+            result = [],
+            tracker = new Tracker(function (type, block) {
+                console.log(type);
+                result.push(blockblog.run(type, block));
+            }),
+            i;
+
+        for (i=0; i<lines.length; ++i) {
+            tracker.append(lines[i]);
+        }
+
+        tracker.flush();
+
+        return result.join('\r\n');
     }
 };
 
--- engine/rules/default.js
+++ engine/rules/default.js
@@ -4,4 +4,13 @@ module.exports = function (blockblog) {
     blockblog.addRule('Default', function (block) {
         return '\t' + block.join('\r\n');
     });
+
+    blockblog.addRule('BigImage', function (block) {
+        return '\t\t' + block.join('\r\n');
+    });
+
+    blockblog.addRule('Code', function (block, param) {
+
+        return '\t\t\t' + block.join('\r\n') + '\r\n' + param;
+    });
 };
